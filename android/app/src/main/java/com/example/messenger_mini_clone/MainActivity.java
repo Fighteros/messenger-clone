655cb3846831fe679679774f2e683bbb3eeabeff
fighteros <fighteros121212@gmail.com>
2021-08-30 06:13:24 +0200

    created basic design, will be refactored

---
+++ android/app/src/main/java/com/example/messenger_mini_clone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.messenger_mini_clone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'modules/login/LoginScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
